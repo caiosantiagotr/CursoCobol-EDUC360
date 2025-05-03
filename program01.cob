51badc2a6980a1cc0d528e9f7a9443163c8305ef
Caio <caiosantiago0987sg@gmail.com>
2025-05-03 15:24:56 -0300

    commit inicial

---
+++ program01.cob
@@ -0,0 +1,22 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+      *=====================================================
+      *==   Autor: Caio              EMPRESA: INFORMATICA
+      *==  Objetivo: 1o Contato com Cobol
+      *==  Data: 03/05/2025
+      *==  Observacoes:
+      *======================================================
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       INPUT-OUTPUT SECTION.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+       DISPLAY "Ola Caio"
+       DISPLAY "Tudo bem"
+       DISPLAY "Boa noite"
+       DISPLAY "Ola Mundo"
+       STOP RUN.
+       END PROGRAM YOUR-PROGRAM-NAME.
