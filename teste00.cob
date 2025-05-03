f74c5f33c20755394719d1fa3770624bfd69f300
Caio <caiosantiago0987sg@gmail.com>
2025-05-03 12:15:57 -0300

    Initial commit

---
+++ teste00.cob
@@ -0,0 +1,21 @@
+
+       IDENTIFICATION DIVISION.
+       *-*-*-*-*-*-*-*-*-*-*-**-*-*-*
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       =============================
+       *** alteraçoes **************
+       =============================
+Caio12* ** Programador: Caio - Data: 03/05/2025
+Caio12* ** Inclusao de x, y variaveis
+      * ** para calculo de salario
+Caio12* ** media de todos os salrios
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+ Caio12 criei algo aqui.
+ Caio12  aqui tambem
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM YOUR-PROGRAM-NAME.
